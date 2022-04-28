31d0f3e269cde175c504f02be90bfc360d0d49b4
Vicky Phung <vickyphung@gmail.com>
2022-04-28 16:15:26 -0400

    removed white space from server.js and passitaround.js

--- ExpressLab/pass-it-around/passitaround.js
+++ ExpressLab/pass-it-around/passitaround.js
@@ -3,8 +3,6 @@ const express = require("express")
 const morgan = require("morgan")
 const bodyParser = require("body-parser")
 
-
-
 const server = express()
 const PORT = process.env.PORT || 5000
 
@@ -18,20 +16,6 @@ server.get("/", (req, res)=>{
     // next()
 })
 
-// link that says "take one down, pass it around"
-// link to `/98`, where the number represents the number of bottles left
-// get "/:number_of_bottles"
-// The number of bottles of beer on the wall (i.e. `98 Bottles of beer on the wall.`)
-// a link to "take one down, pass it around", where the href is number of bottles in the parameter minus 1.
-// - If there are 0 bottles left, do not show a link to "take one down"
-
-// server.get("/:bottleNumber", (req, res)=>{
-//     let bottleNumber = req.params.bottleNumber
-//     res.status(200).json({message: `${counter} Bottles of beer on the wall`,
-//         link: 
-//     })
-// })
-
 server.get("/multiple/:bottleNumber", (req, res)=>{
     let counter = 99 - 1
     let bottleNumber = req.params.bottleNumber
@@ -43,13 +27,11 @@ server.get("/multiple/:bottleNumber", (req, res)=>{
 function bottles (num) {
     let numberBottles = []
     // let bottles = bottles
-
     for(let counter = num; counter >= 1; counter--) 
     {
         // if (counter == 1) {
         //     bottles = 'bottle';
         // }
-
         numberBottles.push(counter + " bottles of beer on the wall. " + counter + " bottles of beer. Take one down. Pass It Around." + (counter-1) + " bottles of beer on the wall" )
     }
     return numberBottles
@@ -61,7 +43,6 @@ server.get("/single/:singleNumber", (req, res)=>{
     res.status(200).json({message: singleNumber + " bottles of beer on the wall. " + singleNumber + " bottles of beer. Take one down. Pass It Around." + (singleNumber-1) + " bottles of beer on the wall", link: `localhost:5000/${singleNumber-1}`
     })
 })
-
 
 server.listen(PORT, ()=>{
     console.log(`Server is listening at: ${PORT}`)
--- ExpressLab/server.js
+++ ExpressLab/server.js
@@ -2,20 +2,15 @@ const express = require("express")
 const morgan = require("morgan")
 const bodyParser = require("body-parser")
 
-
-
 const server = express()
 const PORT = process.env.PORT || 3000
 
 server.use(express.json())
 server.use(bodyParser.json())
 server.use(morgan("dev"))
-
-
 server.get("/", (req, res)=>{
     res.status(200).json({message: "API UP!"})
 })
-
 
 //Greeting Express Application
 server.get("/greeting/:name", (req, res)=>{
@@ -26,9 +21,7 @@ server.get("/greeting/:name", (req, res)=>{
     console.log(name)
 })
 
-
 //Tip Calculator Express Application
-
 server.get("/tip/:total/:tipPercentage", (req, res)=>{
     const total = req.params.total
     const tipPercentage = req.params.tipPercentage / 100
@@ -36,10 +29,8 @@ server.get("/tip/:total/:tipPercentage", (req, res)=>{
     res.status(200).json({message: `Please leave a tip of $${tipTotal}`})
 })
 
-
 //Magic 8 Ball
 server.get("/magic/:question", (req, res)=>{
-
     const question = req.params.question
     const ballResponse = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
     const randomBallResponse = ballResponse[Math.floor(Math.random()*ballResponse.length)];
@@ -47,16 +38,6 @@ server.get("/magic/:question", (req, res)=>{
     res.json({message: `The answer to '${question}?' is: ${randomBallResponse}.`
     })
 })
-
-
-
-
-
-
-
-
-
-
 
 server.listen(PORT, ()=>{
     console.log(`Server is listening at: ${PORT}`)
