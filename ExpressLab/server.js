981f8d4e5e15808f721e4fc125b7e59c05408654
Vicky Phung <vickyphung@gmail.com>
2022-04-28 13:45:29 -0400

    Magic 8 Ball Express Application Created

--- ExpressLab/server.js
+++ ExpressLab/server.js
@@ -37,8 +37,16 @@ server.get("/tip/:total/:tipPercentage", (req, res)=>{
 })
 
 
+//Magic 8 Ball
+server.get("/magic/:question", (req, res)=>{
 
-
+    const question = req.params.question
+    const ballResponse = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    const randomBallResponse = ballResponse[Math.floor(Math.random()*ballResponse.length)];
+    console.log(question, randomBallResponse);
+    res.json({message: `The answer to '${question}?' is: ${randomBallResponse}.`
+    })
+})
 
 
 
